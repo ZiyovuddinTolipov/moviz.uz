2e36f8b6f3b2f8fb35d7aa374ff11e8beaa3048d
tolipov <ziyovuddin.developer@gmail.com>
2024-01-30 00:49:56 +0500

    product

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -11,14 +11,14 @@ const App = () => {
     useEffect(() => {
         gsap.fromTo(elInner('.box'),
             { opacity: 0, scale: 0.5 },
-            { opacity: 1, scale: 1, duration: 2, repeat: 0, yoyo: true, stagger: 1 })
+            { opacity: 1, scale: 1, duration: 1, repeat: 0, yoyo: true, stagger: 1 })
     }, [])
     const styles = {
         videoBox: "w-1/2 flex items-center justify-center"
     }
     return (
-        <section className='h-screen max-w-[1400px] mx-auto w-full ' id='allproduct'>
-            <h1 className='text-3xl text-white w-4/5 text-left font-bold'>  {text.map((el, i) => (
+        <section className='h-screen max-w-[1400px] mx-auto w-full mt-20' id='allproduct'>
+            <h1 className='text-3xl text-white w-4/5 text-left font-bold'> {text.map((el, i) => (
                 <motion.span
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
@@ -31,8 +31,32 @@ const App = () => {
                     {el}{" "}
                 </motion.span>
             ))}</h1>
-            <div ref={el} className='flex flex-row flex-wrap text-white text-xl font-semibold'>
-                <div className='flex box w-1/2 justify-between' >
+            <div ref={el} className='flex gap-[4%] flex-row flex-wrap text-white text-xl font-semibold'>
+                <div className='flex box w-1/2 justify-between basis-[48%] max-w-[50%] mt-[3rem]' >
+                    <div className='w-1/2 flex flex-col gap-3 justify-between py-4 pl-4'>
+                        <div>
+                            <h3 className='text-white'>
+                                Video Type : <span>SMD post animation</span>
+                            </h3>
+                            <h3 className='text-white'>
+                                Video Type : <span>SMD post animation</span>
+                            </h3>
+                            <h3 className='text-white'>
+                                Video Type : <span>SMD post animation</span>
+                            </h3>
+                            <h3 className='text-white'>
+                                Video Type : <span>SMD post animation</span>
+                            </h3>
+                        </div>
+                        <div>
+                            <button>49$</button>
+                        </div>
+                    </div>
+                    <div className={styles.videoBox}>
+                        <img src={moviz1} alt="img" className='h-[300px] rounded-xl' />
+                    </div>
+                </div>
+                <div className='flex box w-1/2 justify-between basis-[48%] max-w-[50%] mt-[3rem]' >
                     <div className='w-1/2 flex flex-col gap-3 justify-center'>
                         <h3 className='text-white'>
                             Video Type : <span>SMD post animation</span>
@@ -54,8 +78,7 @@ const App = () => {
                         <img src={moviz1} alt="img" className='h-[300px] rounded-xl' />
                     </div>
                 </div>
-
-                <div className='flex box w-1/2 justify-between' >
+                <div className='flex box w-1/2 justify-between basis-[48%] max-w-[50%] mt-[3rem]' >
                     <div className='w-1/2 flex flex-col gap-3 justify-center'>
                         <h3 className='text-white'>
                             Video Type : <span>SMD post animation</span>
@@ -77,30 +100,7 @@ const App = () => {
                         <img src={moviz1} alt="img" className='h-[300px] rounded-xl' />
                     </div>
                 </div>
-
-                <div className='flex box w-1/3 justify-between' >
-                    <div className='w-1/2 flex flex-col gap-3 justify-center'>
-                        <h3 className='text-white'>
-                            Video Type : <span>SMD post animation</span>
-                        </h3>
-                        <h3 className='text-white'>
-                            Video Type : <span>SMD post animation</span>
-                        </h3>
-                        <h3 className='text-white'>
-                            Video Type : <span>SMD post animation</span>
-                        </h3>
-                        <h3 className='text-white'>
-                            Video Type : <span>SMD post animation</span>
-                        </h3>
-                        <h3>
-
-                        </h3>
-                    </div>
-                    <div className={styles.videoBox}>
-                        <img src={moviz1} alt="img" className='h-[300px] rounded-xl' />
-                    </div>
-                </div>
-                <div className='flex box w-1/2 justify-between' >
+                <div className='flex box w-1/2 justify-between basis-[48%] max-w-[50%] my-[3rem]' >
                     <div className='w-1/2 flex flex-col gap-3 justify-center'>
                         <h3 className='text-white'>
                             Video Type : <span>SMD post animation</span>
