ecb65d3fe6db2e5ea625181683a35f64d3c02d0e
tolipov <ziyovuddin.developer@gmail.com>
2024-02-04 23:04:06 +0500

    hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ const Navbar = () => {
     return (
         <nav className="sticky z-50 mx-auto  py-3 px-5 my-3 flex items-center justify-between top-3 max-w-[1400px] bg-white/10 backdrop-blur-md w-[90%] rounded-xl">
             <div>
-                <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-8" /></a>
+                <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-4 md:h-8" /></a>
             </div>
-            <ul className="flex text-white gap-3 text-xl font-semibold">
+            <ul className="flex text-white gap-3 text-base md:text-xl font-semibold">
                 <a href="#home"><li>Home</li></a>
                 <a href="#allproduct"><li>All product</li></a>
                 <a href="#about"><li>About</li></a>
