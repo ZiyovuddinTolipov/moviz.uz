a6c6f841ccb1ce8d5d4390ac54f88a6c330ca2c5
tolipov <ziyovuddin.developer@gmail.com>
2024-02-06 20:38:33 +0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logooq from "../assets/oq.png"
+
 const Navbar = () => {
     return (
         <nav className="sticky z-50 mx-auto  py-3 px-5 my-3 flex items-center justify-between top-3 max-w-[1400px] bg-white/10 backdrop-blur-md w-[90%] rounded-xl">
@@ -6,9 +7,9 @@ const Navbar = () => {
                 <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-4 md:h-8" /></a>
             </div>
             <ul className="flex text-white gap-3 text-base md:text-xl font-semibold">
-                <a href="#home"><li>Home</li></a>
-                <a href="#allproduct"><li>All product</li></a>
-                <a href="#about"><li>About</li></a>
+                <a href="#home"><li>Bosh sahifa</li></a>
+                <a href="#allproduct"><li>Tariflarimiz</li></a>
+                <a href="#about"><li>Men haqimda</li></a>
             </ul>
         </nav>
     )
