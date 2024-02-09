28eb47c00e41a5e9c73720e4d36a5f16ae9111e2
tolipov <ziyovuddin.developer@gmail.com>
2024-02-10 01:08:36 +0500

    about image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
     return (
         <section className='w-full border-t-2 border-t-slate-300 pb-4' id='about'>
             <main className='max-w-[1400px] pt-24 mt-5 w-[90%] mx-auto  flex flex-col-reverse justify-center md:flex-row min-h-screen'>
-                <div className="w-full md:w-1/2 text-white flex flex-col gap-5 h-[600px] justify-end">
+                <div className="w-full md:w-1/2 text-white flex flex-col gap-5 h-[600px] justify-start md:justify-end mt-3 md:mt-0">
                     <div className="circle rounded-full absolute " />
                     <h1 className='text-6xl font-bold'>Moviz.Cg</h1>
                     <div className='flex flex-col gap-3 relative z-10'>
@@ -22,9 +22,9 @@ const About = () => {
                         <h3 className='text-2xl font-[700]'>Yo'ldoshboyev  Tolibjon Nurullayevich.</h3>
                     </div>
                     {/* skills */}
-                    <div className='flex about_links mt-10'>
-                        <div className='flex gap-3'>
-                            <div className='w-[100px] '>
+                    <div className='about_links hidden md:flex'>
+                        <div className='flex gap-3 '>
+                            <div className='w-[100px]'>
                                 <div className='w-[70px] mx-auto'>
                                     <CircularProgressbarWithChildren value={90}>
                                         <img src={AdobeP} alt="icon" className='h-10 w-10 rounded-full' />
@@ -73,7 +73,7 @@ const About = () => {
                                     </CircularProgressbarWithChildren>
                                 </div>
                                 <div className='text-center text-[.9rem] mt-1'>
-                                Vn
+                                    Vn
                                 </div>
                             </div>
                             <div className='w-[100px] '>
@@ -83,17 +83,31 @@ const About = () => {
                                     </CircularProgressbarWithChildren>
                                 </div>
                                 <div className='text-center text-[.9rem] mt-1'>
-                                Davenci Resolver
+                                    Davenci Resolver
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div className='block md:hidden text-xl font-semibold '>
+                        <h3 className='mt-2 mb-1'>Adobe Premiere pro</h3>
+                        <div className="range" style={{ '--p': "90" }} />
+                        <h3 className='mt-2 mb-1'>Adobe Photoshop</h3>
+                        <div className="range" style={{ '--p': "66" }} />
+                        <h3 className='mt-2 mb-1'>Adobe After Effect</h3>
+                        <div className="range" style={{ '--p': "60  " }} />
+                        <h3 className='mt-2 mb-1'>Cap Cut</h3>
+                        <div className="range" style={{ '--p': "90  " }} />
+                        <h3 className='mt-2 mb-1'>Vn</h3>
+                        <div className="range" style={{ '--p': "70  " }} />
+                        <h3 className='mt-2 mb-1'>Davenci Resolver</h3>
+                        <div className="range" style={{ '--p': "40  " }} />
+                    </div>
                 </div>
-                <div className="w-full md:w-1/2 flex items-end justify-center h-[600px]">
+                <div className="w-full md:w-1/2 flex items-end justify-center h-[300px] md:h-[600px]">
                     <img src={avatar} alt="img" className='w-[200px] md:w-[400px] ' />
                 </div>
             </main>
-        </section>
+        </section >
     )
 }
 
