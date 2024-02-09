9e3019be03747330a3acaf0af5726526f1af81bd
tolipov <ziyovuddin.developer@gmail.com>
2024-02-10 02:27:02 +0500

    navbar

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,45 +1,70 @@
-import { useState } from 'react'
-import { AiOutlineClose, AiOutlineHeart, AiOutlineLogin, AiOutlineMenu, AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai'
+import { useState } from "react";
 import logooq from "../assets/oq.png"
+import { CiHome } from "react-icons/ci";
 
-const Navbar = () => {
-    const [nav, setNav] = useState(true)
 
-    const handleNav = () => {
-        console.log('hello');
-        // alert('hello');
-        setNav(!nav)
-    }
-    const style = {
-        navbar: `sticky z-50 mx-auto  py-3 px-5 my-3 flex items-center justify-between top-3 max-w-[1400px] bg-white/10 backdrop-blur-md w-[90%] rounded-xl`,
-    }
+export default function NavBar() {
+    const [navbar, setNavbar] = useState(false);
+
     return (
-        <nav className={style.navbar}>
-
-            <div>
-                <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-4 md:h-8" /></a>
-            </div>
-            <ul className=" hidden md:flex text-white gap-3 text-xl font-semibold">
-                <a href="#home"><li>Bosh sahifa</li></a>
-                <a href="#allproduct"><li>Tariflarimiz</li></a>
-                <a href="#about"><li>Men haqimda</li></a>
-            </ul>
-
-            <div onClick={handleNav} className='block md:hidden ease-in-out duration-300 mr-5 cursor-pointer'>
-                {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-            </div>
-            <div className={!nav ? 'text-white text-xl fixed left-0 top-0 w-[80%] bg-[#131b27] backdrop-blur-md min-h-full border-r ease-in-out duration-500' : "fixed left-[-100%]"}>
-                <div className='w-full my-4'>
-                    <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-8" /></a>
+        <nav className="absolute z-50 left-0 right-0 py-3 px-5 my-3 top-3  w-full sticky">
+            <div className="md:justify-between flex  mx-auto  md:items-center flex-col md:flex-row  px-3 md:px-8 max-w-[1400px] bg-white/10 backdrop-blur-md w-[90%] rounded-xl">
+                <div className="w-[100%] md:w-auto">
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <div>
+                            <a href="#"> <img src={logooq} alt="moviz.uz logo moviz.cg instagram " className="h-8" /></a>
+                        </div>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <ul className="flex flex-col md:hidden text-white gap-3 text-xl font-semibold bg-[#131b27] backdrop-blur-md">
-                    <a href="#home" ><li>Bosh sahifa</li></a>
-                    <a href="#allproduct"><li>Tariflarimiz</li></a>
-                    <a href="#about"><li>Men haqimda</li></a>
-                </ul>
+                <div className="w-[100%] md:w-auto">
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                            }`}
+                    >
+                        <ul className="items-start px-3 justify-start flex flex-col md:flex-row  md:flex md:space-x-6 text-xl font-[500] gap-4">
+                            <a href="#home" className="mt-2 text-white"><li>Bosh sahifa</li></a>
+                            <a href="#allproduct" className="mt-2 text-white"><li>Tariflarimiz</li></a>
+                            <a href="#about" className="mt-2 text-white"><li>Men haqimda</li></a>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
-    )
+    );
 }
-
-export default Navbar
